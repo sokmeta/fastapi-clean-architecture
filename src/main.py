54a6832e677ec4e634568af830c21d464e35b37c
sokmeta <sokmeta.reth@quad.co.jp>
2024-07-11 18:36:18 +0900

    fix: import model in alembic

--- src/main.py
+++ src/main.py
@@ -6,21 +6,9 @@ from middlewares import SessionMiddleware
 from contextlib import asynccontextmanager
 
 
-
-@asynccontextmanager
-async def lifespan(application: FastAPI):
-    Base.metadata.create_all(bind=engine)
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.add_middleware(SessionMiddleware)
 
 app.include_router(item.router, prefix=PREFIX)
 app.include_router(category.router, prefix=PREFIX)
-
-# @app.on_event("startup")
-# async def on_startup():
-#     Base.metadata.create_all(bind=engine)
-
